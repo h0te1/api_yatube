d7cc3f4b3b48361123643f56c70c64d86e8a3ef9
Никита Зайцев <genius.zaytsev@gmail.com>
2023-04-04 18:41:01 +0500

    исправил проблемы с комментариями

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,10 +6,11 @@ from posts.models import Comment, Group, Post
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
+    post = serializers.CharField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ('author', 'post', 'text', 'created')
 
 
 class GroupSerializer(serializers.ModelSerializer):
