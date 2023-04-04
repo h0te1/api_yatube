48f3686683dac7d53405aff0c8e1e6ea0fdece36
Никита Зайцев <genius.zaytsev@gmail.com>
2023-04-04 23:30:14 +0500

    ещ1 один тест

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 from rest_framework import viewsets
 
-from posts.models import Group, Post, Comment
+from posts.models import Group, Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer,
                           GroupSerializer, PostSerializer)
@@ -36,15 +36,17 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        serializer.save(author=self.request.user, post=post)
-        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        created = serializer.save(author=self.request.user, post=post)
+        return created
 
     def perform_update(self, serializer):
-        comment = get_object_or_404(Comment, id=self.kwargs.get('comment_id'))
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        comment = post.comments.get(id=self.kwargs.get('comment_id'))
         serialized = serializer(comment, data=self.request.data, partial=True)
         serialized.save()
-        return get_object_or_404(Comment, id=self.kwargs.get('comment_id'))
+        return post.comments.get(id=self.kwargs.get('comment_id'))
 
     def destroy(self):
-        instance = get_object_or_404(Comment, id=self.kwargs.get('comment_id'))
-        self.perform_destroy(instance)
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        comment = post.comments.get(id=self.kwargs.get('comment_id'))
+        self.perform_destroy(comment)
