83bb5b5d1b77a0688ebdc45a851addd04f23afe9
Никита Зайцев <genius.zaytsev@gmail.com>
2023-04-05 01:33:35 +0500

    убрал строку в сериализаторе

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,7 +6,6 @@ from posts.models import Comment, Group, Post
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
-    post = serializers.CharField(read_only=True)
 
     class Meta:
         model = Comment
