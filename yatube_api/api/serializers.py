a2a730d8e79773de9b958306ea6bbaf1cf74cf79
Никита Зайцев <genius.zaytsev@gmail.com>
2023-04-05 01:53:59 +0500

    добавил post в поля явно

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -9,7 +9,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('author', 'text', 'created', 'id')
+        fields = ('author', 'text', 'created', 'id', 'post')
         read_only_fields = ['post']
 
 
